aff3625be071a81285f31e0a88a4f946d00e09b4
cloudy9101 <cloudy9101@163.com>
2013-12-01 17:46:07 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match 'signin', to: 'sessions#new', via: 'get'
+  match 'signout', to: 'sessions#destroy', via: 'delete'
   match '/signup', to: 'users#new', via: 'get'
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
